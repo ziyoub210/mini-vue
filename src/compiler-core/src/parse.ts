416650691bfa24ce7bca22bad4a2e84947b0d296
ziyoub210 <ziyoub210@163.com>
2023-08-27 19:48:04 +0800

    feat: 解析element 和 text

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,4 +1,8 @@
 import { NodeTypes } from './ast';
+enum TagType {
+  START,
+  END,
+}
 export function baseParse(content: string) {
   const context = createParseContext(content);
   return createRoot(parseChildren(context));
@@ -12,13 +16,59 @@ function createRoot(children) {
 
 function parseChildren(context) {
   const nodes: any = [];
-  if (context.source.startsWith('{{')) {
-    const node = parseInterpolation(context);
-    console.log(node, 'node');
-    nodes.push(node);
+
+  let node;
+  const s = context.source;
+  if (s.startsWith('{{')) {
+    node = parseInterpolation(context);
+  } else if (s[0] === '<') {
+    if (/[a-z]/.test(s[1])) {
+      node = parseElement(context);
+    }
   }
+  if (!node) {
+    node = parseText(context);
+  }
+  nodes.push(node);
 
   return nodes;
+}
+
+function parseText(context) {
+  //1. 获取content
+  const content = parseTextData(context, context.source.length);
+
+  //2. 推进
+  advanceBy(context, content.length);
+
+  return {
+    type: NodeTypes.TEXT,
+    content,
+  };
+}
+function parseTextData(context, length) {
+  return context.source.slice(0, length);
+}
+
+function parseElement(context) {
+  const element = parseTag(context, TagType.START);
+  parseTag(context, TagType.END);
+  return element;
+}
+
+function parseTag(context, type: TagType) {
+  //解析 tag
+  //删除处理完成的代码
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+  if (type === TagType.START) {
+    return {
+      type: NodeTypes.ELEMENT,
+      tag,
+    };
+  }
 }
 
 function parseInterpolation(context: any) {
@@ -30,9 +80,10 @@ function parseInterpolation(context: any) {
 
   const rawContentLength = closeIndex - openDelemiter.length;
 
-  const rawContent = context.source.slice(0, rawContentLength);
+  // const rawContent = context.source.slice(0, rawContentLength);
+  const rawContent = parseTextData(context, rawContentLength);
   const content = rawContent.trim();
-  console.log(rawContent, 'source');
+
   advanceBy(context, rawContentLength + closeDelemiter.length);
 
   return {
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -5,13 +5,30 @@ describe('parse', () => {
   describe('interpolation', () => {
     test('simple interpolation', () => {
       const ast = baseParse('{{ message }}');
-      console.log(ast.children[0], 'ast.children[0]ast.children[0]ast.children[0]');
       expect(ast.children[0]).toStrictEqual({
         type: NodeTypes.INTERPOLATION,
         content: {
           type: NodeTypes.SIMPLE_EXPRESSION,
           content: 'message',
         },
+      });
+    });
+  });
+  describe('element', () => {
+    it('simple element div', () => {
+      const ast = baseParse('<div></div>');
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: 'div',
+      });
+    });
+  });
+  describe('text', () => {
+    it('simple  text', () => {
+      const ast = baseParse('some text');
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.TEXT,
+        content: 'some text',
       });
     });
   });
